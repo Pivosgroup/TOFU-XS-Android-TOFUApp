891154adecb285dc3b2634f89b7683f7349be4cc
davilla <davilla@4pi.com>
2012-12-23 12:39:05 -0500

    [aml] add PivosPowerSyscall
    This handles M1/M3 suspend/resume/shutdown. On M3 users will have to
    physically remove power to power off. At least the S95-xbmc script has a
    chance to run and dismount drives and removable media.

---
+++ xbmc/powermanagement/linux/PivosPowerSyscall.h
@@ -0,0 +1,51 @@
+/*
+ *      Copyright (C) 2012 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "powermanagement/IPowerSyscall.h"
+
+class CPivosPowerSyscall : public IPowerSyscall
+{
+public:
+  CPivosPowerSyscall();
+  virtual ~CPivosPowerSyscall() {}
+
+  virtual bool Powerdown();
+  virtual bool Suspend();
+  virtual bool Hibernate();
+  virtual bool Reboot();
+
+  virtual bool CanPowerdown();
+  virtual bool CanSuspend();
+  virtual bool CanHibernate();
+  virtual bool CanReboot();
+  virtual int  BatteryLevel();
+  virtual bool PumpPowerEvents(IPowerEventsCallback *callback);
+
+  static bool HasPivosPowerSyscall();
+
+private:
+  bool m_CanPowerdown;
+  bool m_CanSuspend;
+  bool m_CanHibernate;
+  bool m_CanReboot;
+
+  bool m_OnResume;
+  bool m_OnSuspend;
+};
