b9857ad1a3d65cd5da48ee63cc35059ef7b84d3b
Cory Fields <theuni-nospam-@xbmc.org>
2012-12-27 00:42:36 -0500

    [aml] fix busted locales in linux, probably still broken in android
    Combined with the newly installed glibc locale data, this fixes i18n issues
    related to text processing. Fixes, for example, uppercase non-ascii characters
    at the confluence home screen.

--- xbmc/LangInfo.cpp
+++ xbmc/LangInfo.cpp
@@ -170,7 +170,7 @@ void CLangInfo::CRegion::SetGlobalLocale()
   // decimal separator is changed depending of the current language
   // (ie. "," in French or Dutch instead of "."). This breaks atof() and
   // others similar functions.
-#if defined(TARGET_FREEBSD) || defined(TARGET_DARWIN_OSX)
+#if defined(TARGET_FREEBSD) || defined(TARGET_DARWIN_OSX) || defined(TARGET_LINUX)
   // on FreeBSD and darwin libstdc++ is compiled with "generic" locale support
   if (setlocale(LC_COLLATE, strLocale.c_str()) == NULL
   || setlocale(LC_CTYPE, strLocale.c_str()) == NULL)
