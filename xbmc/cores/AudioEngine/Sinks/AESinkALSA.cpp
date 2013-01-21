985f89b588cd756e8f186003a499ae2519a33ab6
davilla <davilla@4pi.com>
2013-01-21 11:30:47 -0500

    [aml] fixed, alsa/kernel lies, so map everything to 44100 or 48000

--- xbmc/cores/AudioEngine/Sinks/AESinkALSA.cpp
+++ xbmc/cores/AudioEngine/Sinks/AESinkALSA.cpp
@@ -307,6 +307,28 @@ bool CAESinkALSA::InitializeHW(const ALSAConfig &inconfig, ALSAConfig &outconfig
 
   unsigned int sampleRate   = inconfig.sampleRate;
   unsigned int channelCount = inconfig.channels;
+#if defined(HAS_AMLPLAYER) || defined(HAS_LIBAMCODEC)
+  // alsa/kernel lies, so map everything to 44100 or 48000
+  switch(sampleRate)
+  {
+    case 11025:
+    case 22050:
+    case 88200:
+    case 176400:
+      sampleRate = 44100;
+      break;
+    case 8000:
+    case 16000:
+    case 24000:
+    case 32000:
+    case 96000:
+    case 192000:
+    case 384000:
+      sampleRate = 48000;
+      break;
+  }
+#endif
+
   snd_pcm_hw_params_set_rate_near    (m_pcm, hw_params, &sampleRate, NULL);
   snd_pcm_hw_params_set_channels_near(m_pcm, hw_params, &channelCount);
 
