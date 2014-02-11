c7552d80be224a06643cc12cb9ddcad4db01b8aa
Bl4ck09 <bl4ck09@me.com>
2014-02-11 20:20:59 +0100

    Remove view flickering (view cycle) on addon launch

--- xbmc/filesystem/PluginDirectory.cpp
+++ xbmc/filesystem/PluginDirectory.cpp
@@ -464,7 +464,6 @@ bool CPluginDirectory::WaitOnScriptResult(const CStdString &scriptPath, int scri
   unsigned int startTime = XbmcThreads::SystemClockMillis();
   CGUIDialogProgress *progressBar = NULL;
   bool cancelled = false;
-  bool inMainAppThread = g_application.IsCurrentThread();
 
   CLog::Log(LOGDEBUG, "%s - waiting on the %s (id=%d) plugin...", __FUNCTION__, scriptName.c_str(), scriptId);
   while (true)
@@ -520,10 +519,6 @@ bool CPluginDirectory::WaitOnScriptResult(const CStdString &scriptPath, int scri
         m_cancelled = true;
       }
     }
-    else // if the progressBar exists and we call StartModal or Progress we get the
-         //  ProcessRenderLoop call anyway.
-      if (inMainAppThread) 
-        g_windowManager.ProcessRenderLoop();
 
     if (!cancelled && m_cancelled)
     {
