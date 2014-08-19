picostav/xbmc

pivos: change branding from xbmc to tofu

xbmc/android/activity/android_main.cpp
@@ -55,14 +55,14 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
   if (vm->GetEnv(reinterpret_cast<void**>(&env), version) != JNI_OK)
     return -1;
 
-  jclass cMain = env->FindClass("org/xbmc/xbmc/XBMCBroadcastReceiver");
+  jclass cMain = env->FindClass("com/pivos/tofu/XBMCBroadcastReceiver");
   if(cMain)
   {
     JNINativeMethod mOnReceive =   { "_onReceive",     "(Landroid/content/Intent;)V", (void*)&CJNIBroadcastReceiver::_onReceive};
     env->RegisterNatives(cMain, &mOnReceive, 1);
   }
 
-  jclass cBroadcastReceiver = env->FindClass("org/xbmc/xbmc/Main");
+  jclass cBroadcastReceiver = env->FindClass("com/pivos/tofu/Main");
   if(cBroadcastReceiver)
   {
     static JNINativeMethod sMethods[] =

Pivosgroup/TOFU-XS-Android-TOFUApp

xbmc/android/activity/android_main.cpp
@@ -82,7 +82,7 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
   if (vm->GetEnv(reinterpret_cast<void**>(&env), version) != JNI_OK)
     return -1;
 
-  jclass cMain = env->FindClass("org/xbmc/xbmc/Main");
+  jclass cMain = env->FindClass("com/pivos/tofu/Main");
   if(cMain)
   {
     static JNINativeMethod sMethods[] = {
@@ -100,7 +100,7 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
     env->RegisterNatives(cMain, &sMethods[0], sizeof(sMethods)/sizeof(JNINativeMethod));
   }
 
-  jclass cBroadcastReceiver = env->FindClass("org/xbmc/xbmc/XBMCBroadcastReceiver");
+  jclass cBroadcastReceiver = env->FindClass("com/pivos/tofu/XBMCBroadcastReceiver");
   if(cBroadcastReceiver)
   {
     JNINativeMethod mOnReceive =  {
@@ -111,7 +111,7 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
     env->RegisterNatives(cBroadcastReceiver, &mOnReceive, 1);
   }
 
-  jclass cFrameAvailableListener = env->FindClass("org/xbmc/xbmc/XBMCOnFrameAvailableListener");
+  jclass cFrameAvailableListener = env->FindClass("com/pivos/tofu/XBMCOnFrameAvailableListener");
   if(cFrameAvailableListener)
   {
     JNINativeMethod mOnFrameAvailable = {